a48f8e190fdb4f0897268efd01bcbcb270daa718
supermarkioner <supermarkioner@gmail.com>
2015-12-02 16:59:01 +1100

    implement function pass-by-pointer, recur strategy, divide and conquer strategy

---
+++ recur.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+void countdown(int n);
+
+int main()
+{
+	countdown(4);
+	return 0;
+}
+
+void countdown(int n)
+{
+	using namespace std;
+	cout << "Counting down ... " << n << " (n at " << &n << ")" << endl;
+	if (n > 0) {
+		countdown(n - 1);
+	}
+	cout << n << ": Kaboom!" << "	(n at " << &n << ")" << endl;
+}
