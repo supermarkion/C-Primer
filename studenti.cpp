d742b4c6a01916f5dbd304539fb1666f6a022cfe
supermarkioner <supermarkioner@gmail.com>
2015-12-08 14:53:38 +1100

    change the implement method of student class

---
+++ studenti.cpp
@@ -0,0 +1,71 @@
+// studenti.cpp -- Student class using private inheritance
+#include "studenti.h"
+using std::ostream;
+using std::endl;
+using std::istream;
+using std::string;
+
+// public methods
+double Student::Average() const
+{
+	if (ArrayDb::size() > 0) {
+		return ArrayDb::sum()/ArrayDb::size();
+	} else {
+		return 0;
+	}
+}
+
+const string & Student::Name() const
+{
+	return (const string &) *this;
+}
+
+double & Student::operator[](int i)
+{
+	return ArrayDb::operator[](i);
+}
+
+double Student::operator[](int i) const
+{
+	return ArrayDb::operator[](i);
+}
+
+ostream & Student::arr_out(ostream & os) const
+{
+	int i;
+	int lim = ArrayDb::size();
+	if (lim > 0) {
+		for (i = 0; i < lim; i++) {
+			os << ArrayDb::operator[](i) << " ";
+			if (i % 5 == 4) {
+				os << endl;
+			}
+		}
+		if (i % 5 != 0) {
+			os << endl;
+		}
+	} else {
+		os << " empty array ";
+	}
+	return os;
+}
+
+// use string version of operator>>()
+istream & operator>>(istream & is, Student & stu)
+{
+	is >> (string &)stu;
+	return is;
+}
+
+istream & getline(istream & is, Student & stu)
+{
+	getline(is, (string &)stu);
+	return is;
+}
+
+ostream & operator<<(ostream & os, const Student & stu)
+{
+	os << "Scores for " << (const string &)stu << ":\n";
+	stu.arr_out(os);	// use private method for scores
+	return os;
+}
